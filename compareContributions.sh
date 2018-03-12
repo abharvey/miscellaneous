be1bc79cc2da1a80d0223c8108a19525d3884f9e
abharvey <aharvey@prontoforms.com>
2018-03-12 14:36:53 -0400

    combined repo stats scripts

--- compareContributions.sh
+++ compareContributions.sh
@@ -36,8 +36,8 @@ else
   done
 fi
 
-#git clone --depth 1 "$1" temp-linecount-repo &&
-#  printf "('temp-linecount-repo' will be deleted automatically)\n\n\n" &&
-#  cloc temp-linecount-repo &&
-#  rm -rf temp-linecount-repo
+git clone --depth 1 $(git config --get remote.origin.url) temp-linecount-repo &&
+  printf "('temp-linecount-repo' will be deleted automatically)\n\n\n" &&
+  cloc temp-linecount-repo &&
+  rm -rf temp-linecount-repo
 

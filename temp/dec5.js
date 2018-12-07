5b7284a4498942266e43a905202e49aab085673e
Alex Harvey <alex.harvey@unb.ca>
2018-12-07 08:43:01 -0500

    Update dec5.js

--- temp/dec5.js
+++ temp/dec5.js
@@ -1,51 +1,5 @@
 const { getInput } = require("./readInput");
 
-const mergeFormPolymer = polymer => {
-    if (polymer.length === 1) {
-        return polymer;
-    }
-
-    let middle = Math.floor(polymer.length / 2);
-    let left = polymer.slice(0, middle);
-    let right = polymer.slice(middle);
-
-    return mergeFormPolymer(
-        formPolymer(left),
-        formPolymer(right)
-    )
-}
-
-function mergePoly(left, right) {
-    let poly = [];
-    if (left.length > 0 && right.length > 0) {
-        if (left[left.length - 1].toUpperCase() === right[0].toUpperCase() && left[left.length - 1] !== right[0]) {
-            left = left.slice(0, left.length - 1);
-            right = right.slice(1);
-        }
-    }
-
-    return poly.concat(left).concat(right);
-}
-
-function formPolymerWhile1(polymer) {
-    let left = [];
-    left.push(polymer[0]);
-    polymer = polymer.slice(1);
-    while (polymer.length > 0) {
-        if (left.length < 1) {
-            left.push(polymer[0]);
-            polymer = polymer.slice(1);
-        } else if (left[left.length - 1].toUpperCase() === polymer[0].toUpperCase() && left[left.length - 1] !== polymer[0]) {
-            left = left.slice(0, left.length - 1);
-            polymer = polymer.slice(1);
-        } else {
-            left.push(polymer[0]);
-            polymer = polymer.slice(1);
-        }
-    }
-    return polymer;
-}
-
 function shouldReact(left, polymer) {
     const leftChar = left[left.length - 1];
     const rightChar = polymer[0];
